aebb82e6430c3925cefd39c62aec1f63e03e3b41
Tutul Kabir <tutulkabir@gmail.com>
2023-03-22 21:40:55 +0530

    form validation

---
+++ react-form-validation-controlled -uncontrolled/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ControlledForm from "./ControlledForm";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Controlled form</h1>
+      <ControlledForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ react-form-validation-controlled/src/ControlledForm.js
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+
+const initialData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  gender: "male",
+  profession: "",
+};
+const ControlledForm = () => {
+  const [userData, setUserData] = useState(initialData);
+
+  const [errors, setErrors] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  const { firstName, lastName, email, gender, profession } = userData;
+
+  // onChange event
+  const handleChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors({
+      ...errors,
+      [e.target.name]: "",
+    });
+  };
+
+  // onSubmit event
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userErrors = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      profession: "",
+    };
+
+    let isError = false;
+    if (firstName === "") {
+      isError = true;
+      userErrors.firstName = "FirstName is Required";
+    }
+    if (lastName === "") {
+      isError = true;
+      userErrors.lastName = "lastName is Required";
+    }
+
+    /* eslint-disable-next-line */
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email === "" || !regexEmail.test(email)) {
+      isError = true;
+      userErrors.email = "email is Required and must be valid";
+    }
+
+    if (profession === "") {
+      isError = true;
+      userErrors.profession = "Profession is Required";
+    }
+
+    setErrors(userErrors);
+
+    // if (userErrors.values().some((elm) => elm.length > 0)) {
+    //   return;
+    // }
+
+    if (isError) {
+      return;
+    }
+    // form is valid, now you can Submit form
+    // submit form
+    setSubmitted(true);
+    setUserData(initialData);
+  };
+
+  return (
+    <div style={{ width: 300, margin: "0 auto" }}>
+      {submitted && (
+        <h3 style={{ color: "green" }}>Form is submitted successfully</h3>
+      )}
+
+      {!submitted && (
+        <form action="" onSubmit={handleSubmit} noValidate>
+          <div>
+            <label htmlFor="firstName">FirstName: </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={handleChange}
+            />
+            <p style={{ color: "red" }}>{errors && errors.firstName}</p>
+          </div>
+
+          <br />
+          <div>
+            <label htmlFor="lastName">LastName: </label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              value={lastName}
+              onChange={handleChange}
+            />
+            <p style={{ color: "red" }}>{errors && errors.lastName}</p>
+          </div>
+
+          <br />
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={handleChange}
+            />
+            <p style={{ color: "red" }}>{errors && errors.email}</p>
+          </div>
+
+          <br />
+          <div>
+            <label htmlFor="gender">Gender: </label>
+            <input
+              type="radio"
+              name="gender"
+              id="gender"
+              checked={gender === "male"}
+              value="male"
+              onChange={handleChange}
+            />
+            Male
+            <input
+              type="radio"
+              name="gender"
+              id="gender"
+              checked={gender === "female"}
+              value="female"
+              onChange={handleChange}
+            />
+            Female
+            <input
+              type="radio"
+              name="gender"
+              id="gender"
+              checked={gender === "other"}
+              value="other"
+              onChange={handleChange}
+            />
+            Other
+          </div>
+          <br />
+
+          <div>
+            <label htmlFor="profession">Profession:</label>
+            <select
+              name="profession"
+              id="profession"
+              value={profession}
+              onChange={handleChange}
+            >
+              <option value="" disabled>
+                Select Option
+              </option>
+              <option value="softDev">Software Developer</option>
+              <option value="dataScientist">Data Scientist</option>
+              <option value="teacher">Teacher</option>
+              <option value="devOps">DevOps Engineer</option>
+            </select>
+            <p style={{ color: "red" }}>{errors && errors.profession}</p>
+          </div>
+
+          <br />
+
+          <input type="submit" value="Submit" />
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default ControlledForm;
